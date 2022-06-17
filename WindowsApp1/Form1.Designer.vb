5b5cf84b917a46f19f32cb1589d178f51f62fd96
Ruslan <78550530+Roller231@users.noreply.github.com>
2022-06-17 17:29:40 +0300

    CalculateOnBasic
    This just easy calculate on VisualBasic

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Zero = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Result = New System.Windows.Forms.Button()
+        Me.One = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Three = New System.Windows.Forms.Button()
+        Me.Two = New System.Windows.Forms.Button()
+        Me.Four = New System.Windows.Forms.Button()
+        Me.Five = New System.Windows.Forms.Button()
+        Me.Six = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Delenie = New System.Windows.Forms.Button()
+        Me.Nine = New System.Windows.Forms.Button()
+        Me.Eight = New System.Windows.Forms.Button()
+        Me.Seven = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.AC = New System.Windows.Forms.Button()
+        Me.LabelAns = New System.Windows.Forms.Label()
+        Me.Drob = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Zero
+        '
+        Me.Zero.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Zero.Location = New System.Drawing.Point(12, 404)
+        Me.Zero.Name = "Zero"
+        Me.Zero.Size = New System.Drawing.Size(66, 62)
+        Me.Zero.TabIndex = 0
+        Me.Zero.Text = "0"
+        Me.Zero.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(601, 197)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 2
+        '
+        'Result
+        '
+        Me.Result.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Result.Location = New System.Drawing.Point(156, 404)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(143, 62)
+        Me.Result.TabIndex = 3
+        Me.Result.Text = "="
+        Me.Result.UseVisualStyleBackColor = True
+        '
+        'One
+        '
+        Me.One.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.One.Location = New System.Drawing.Point(12, 333)
+        Me.One.Name = "One"
+        Me.One.Size = New System.Drawing.Size(66, 65)
+        Me.One.TabIndex = 6
+        Me.One.Text = "1"
+        Me.One.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button3.Location = New System.Drawing.Point(227, 333)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(66, 65)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "+"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Three
+        '
+        Me.Three.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Three.Location = New System.Drawing.Point(156, 333)
+        Me.Three.Name = "Three"
+        Me.Three.Size = New System.Drawing.Size(66, 65)
+        Me.Three.TabIndex = 8
+        Me.Three.Text = "3"
+        Me.Three.UseVisualStyleBackColor = True
+        '
+        'Two
+        '
+        Me.Two.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Two.Location = New System.Drawing.Point(84, 333)
+        Me.Two.Name = "Two"
+        Me.Two.Size = New System.Drawing.Size(66, 65)
+        Me.Two.TabIndex = 9
+        Me.Two.Text = "2"
+        Me.Two.UseVisualStyleBackColor = True
+        '
+        'Four
+        '
+        Me.Four.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Four.Location = New System.Drawing.Point(12, 262)
+        Me.Four.Name = "Four"
+        Me.Four.Size = New System.Drawing.Size(66, 65)
+        Me.Four.TabIndex = 10
+        Me.Four.Text = "4"
+        Me.Four.UseVisualStyleBackColor = True
+        '
+        'Five
+        '
+        Me.Five.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Five.Location = New System.Drawing.Point(84, 262)
+        Me.Five.Name = "Five"
+        Me.Five.Size = New System.Drawing.Size(66, 65)
+        Me.Five.TabIndex = 11
+        Me.Five.Text = "5"
+        Me.Five.UseVisualStyleBackColor = True
+        '
+        'Six
+        '
+        Me.Six.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Six.Location = New System.Drawing.Point(156, 262)
+        Me.Six.Name = "Six"
+        Me.Six.Size = New System.Drawing.Size(66, 65)
+        Me.Six.TabIndex = 12
+        Me.Six.Text = "6"
+        Me.Six.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button10.Location = New System.Drawing.Point(228, 262)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(66, 65)
+        Me.Button10.TabIndex = 13
+        Me.Button10.Text = "-"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Delenie
+        '
+        Me.Delenie.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Delenie.Location = New System.Drawing.Point(156, 120)
+        Me.Delenie.Name = "Delenie"
+        Me.Delenie.Size = New System.Drawing.Size(138, 65)
+        Me.Delenie.TabIndex = 14
+        Me.Delenie.Text = "/"
+        Me.Delenie.UseVisualStyleBackColor = True
+        '
+        'Nine
+        '
+        Me.Nine.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Nine.Location = New System.Drawing.Point(156, 191)
+        Me.Nine.Name = "Nine"
+        Me.Nine.Size = New System.Drawing.Size(66, 65)
+        Me.Nine.TabIndex = 15
+        Me.Nine.Text = "9"
+        Me.Nine.UseVisualStyleBackColor = True
+        '
+        'Eight
+        '
+        Me.Eight.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Eight.Location = New System.Drawing.Point(84, 191)
+        Me.Eight.Name = "Eight"
+        Me.Eight.Size = New System.Drawing.Size(66, 65)
+        Me.Eight.TabIndex = 16
+        Me.Eight.Text = "8"
+        Me.Eight.UseVisualStyleBackColor = True
+        '
+        'Seven
+        '
+        Me.Seven.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Seven.Location = New System.Drawing.Point(12, 191)
+        Me.Seven.Name = "Seven"
+        Me.Seven.Size = New System.Drawing.Size(66, 65)
+        Me.Seven.TabIndex = 17
+        Me.Seven.Text = "7"
+        Me.Seven.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button15.Location = New System.Drawing.Point(228, 191)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(66, 65)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "X"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'AC
+        '
+        Me.AC.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.AC.Location = New System.Drawing.Point(12, 120)
+        Me.AC.Name = "AC"
+        Me.AC.Size = New System.Drawing.Size(138, 65)
+        Me.AC.TabIndex = 18
+        Me.AC.Text = "AC"
+        Me.AC.UseVisualStyleBackColor = True
+        '
+        'LabelAns
+        '
+        Me.LabelAns.AutoSize = True
+        Me.LabelAns.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.LabelAns.Location = New System.Drawing.Point(12, 47)
+        Me.LabelAns.Name = "LabelAns"
+        Me.LabelAns.Size = New System.Drawing.Size(0, 31)
+        Me.LabelAns.TabIndex = 19
+        '
+        'Drob
+        '
+        Me.Drob.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Drob.Location = New System.Drawing.Point(84, 404)
+        Me.Drob.Name = "Drob"
+        Me.Drob.Size = New System.Drawing.Size(66, 62)
+        Me.Drob.TabIndex = 20
+        Me.Drob.Text = ","
+        Me.Drob.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 471)
+        Me.Controls.Add(Me.Drob)
+        Me.Controls.Add(Me.LabelAns)
+        Me.Controls.Add(Me.AC)
+        Me.Controls.Add(Me.Seven)
+        Me.Controls.Add(Me.Eight)
+        Me.Controls.Add(Me.Nine)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Delenie)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Six)
+        Me.Controls.Add(Me.Five)
+        Me.Controls.Add(Me.Four)
+        Me.Controls.Add(Me.Two)
+        Me.Controls.Add(Me.Three)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.One)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Zero)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Zero As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Result As Button
+    Friend WithEvents One As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Three As Button
+    Friend WithEvents Two As Button
+    Friend WithEvents Four As Button
+    Friend WithEvents Five As Button
+    Friend WithEvents Six As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Delenie As Button
+    Friend WithEvents Nine As Button
+    Friend WithEvents Eight As Button
+    Friend WithEvents Seven As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents AC As Button
+    Friend WithEvents LabelAns As Label
+    Friend WithEvents Drob As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim x, y, z As Double
+    Dim znak As Integer
+
+    Private Sub Two_Click(sender As Object, e As EventArgs) Handles Two.Click
+        LabelAns.Text = LabelAns.Text + "2"
+    End Sub
+
+    Private Sub Three_Click(sender As Object, e As EventArgs) Handles Three.Click
+        LabelAns.Text = LabelAns.Text + "3"
+    End Sub
+
+    Private Sub Four_Click(sender As Object, e As EventArgs) Handles Four.Click
+        LabelAns.Text = LabelAns.Text + "4"
+    End Sub
+
+    Private Sub Five_Click(sender As Object, e As EventArgs) Handles Five.Click
+        LabelAns.Text = LabelAns.Text + "5"
+    End Sub
+
+    Private Sub Six_Click(sender As Object, e As EventArgs) Handles Six.Click
+        LabelAns.Text = LabelAns.Text + "6"
+    End Sub
+
+    Private Sub Seven_Click(sender As Object, e As EventArgs) Handles Seven.Click
+        LabelAns.Text = LabelAns.Text + "7"
+    End Sub
+
+    Private Sub Eight_Click(sender As Object, e As EventArgs) Handles Eight.Click
+        LabelAns.Text = LabelAns.Text + "8"
+    End Sub
+
+    Private Sub Nine_Click(sender As Object, e As EventArgs) Handles Nine.Click
+        LabelAns.Text = LabelAns.Text + "9"
+    End Sub
+
+    Private Sub Drob_Click(sender As Object, e As EventArgs) Handles Drob.Click
+        LabelAns.Text = LabelAns.Text + ","
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        x = LabelAns.Text
+        LabelAns.Text = ""
+        znak = 1
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        x = LabelAns.Text
+        LabelAns.Text = ""
+        znak = 2
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        x = LabelAns.Text
+        LabelAns.Text = ""
+        znak = 3
+    End Sub
+
+    Private Sub Delenie_Click(sender As Object, e As EventArgs) Handles Delenie.Click
+        x = LabelAns.Text
+        LabelAns.Text = ""
+        znak = 4
+    End Sub
+
+    Private Sub AC_Click(sender As Object, e As EventArgs) Handles AC.Click
+        x = 0
+        y = 0
+        z = 0
+        znak = 0
+        LabelAns.Text = ""
+    End Sub
+
+    Private Sub Result_Click(sender As Object, e As EventArgs) Handles Result.Click
+        y = LabelAns.Text
+        If znak = 1 Then z = x + y
+        If znak = 2 Then z = x - y
+        If znak = 3 Then z = x * y
+        If znak = 4 Then z = x / y
+        LabelAns.Text = z
+
+    End Sub
+
+    Private Sub One_Click(sender As Object, e As EventArgs) Handles One.Click
+        LabelAns.Text = LabelAns.Text + "1"
+    End Sub
+
+    Private Sub Zero_Click(sender As Object, e As EventArgs) Handles Zero.Click
+        LabelAns.Text = LabelAns.Text + "0"
+    End Sub
+End Class
